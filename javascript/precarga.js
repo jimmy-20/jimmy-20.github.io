0230b5c736567d9fc7f75e7f2387e48562a06771
Jaso <80351997+jimmy-20@users.noreply.github.com>
2023-06-11 23:33:34 -0600

    Add files via upload

---
+++ javascript/precarga.js
@@ -0,0 +1,5 @@
+function preCarga(){
+    let contenedor_precarga = document.getElementById('contenedor-precarga');
+    contenedor_precarga.style.visibility='hidden';
+    contenedor_precarga.style.opacity=0;
+}
